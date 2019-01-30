9f67da9195f01b7588da0bfbccffe2e0d256a1a8
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-30 13:25:10 -0500

    refactored Space componentDidMount - also attempted raycaster but will try to step back, delete code that is not working

--- client/components/Space.js
+++ client/components/Space.js
@@ -30,9 +30,14 @@ export default class Space extends React.Component {
     this.start = this.start.bind(this)
     this.stop = this.stop.bind(this)
     this.animate = this.animate.bind(this)
+    this.onWindowResize = this.onWindowResize.bind(this)
+    this.createUniverse = this.createUniverse.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
+    this.onDocumentMouseDown = this.onDocumentMouseDown.bind(this)
   }
 
   componentDidMount() {
+    // === window width & height  ===
     const width = window.innerWidth
     const height = window.innerHeight
 
@@ -41,30 +46,90 @@ export default class Space extends React.Component {
     const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100000)
     const renderer = new THREE.WebGLRenderer({antialias: true})
 
+    this.scene = scene
+    this.camera = camera
+    this.renderer = renderer
+
+    // === orbit controls allows user to navigate 3D space with mouse ===
+    const controls = new OrbitControls(camera, renderer.domElement)
+
     // === raycaster ===
     //Raycasting is used for mouse picking (working out what objects in the 3d space the mouse is over)
     const raycaster = new THREE.Raycaster()
-    const mouse = new THREE.Vector2()
+    let mouse = new THREE.Vector2(),
+      INTERSECTED
 
-    // === resizes browser window ===
-    window.addEventListener(
-      'resize',
-      function() {
-        renderer.setSize(window.innerWidth, window.innerHeight)
-        camera.aspect = window.innerWidth / window.innerHeight
-        camera.updateProjectionMatrix()
-      },
-      false
-    )
+    this.raycaster = raycaster
+    this.mouse = mouse
+
+    // === event listeners ===
+    document.addEventListener('mousemove', this.onMouseMove, false)
+    document.addEventListener('mousedown', this.onDocumentMouseDown, false)
+    window.addEventListener('resize', this.onWindowResize, false)
 
     // === camera settings ===
-    // camera.position.set(0, 0, 10)
     camera.position.z = 10
-    // camera.rotation.z = 90 * Math.PI / 180
+
+    // === renderer  settings ===
+    // renderer displays your beautifully crafted scenes using WebGL
+    renderer.setSize(width, height)
+    renderer.setClearColor('#4776e6')
+    // renderer.domElement.addEventListener('click', raycast, false)
+
+    // === adds background & planets to the scene ===
+    this.createUniverse()
+
+    // === appends scene to the DOM ===
+    this.mount.appendChild(this.renderer.domElement)
+
+    // === start scene ===
+    this.start()
+  }
+
+  componentWillUnmount() {
+    this.stop()
+    this.mount.removeChild(this.renderer.domElement)
+  }
+
+  onMouseMove() {
+    this.mouse.x = event.clientX / window.innerWidth * 2 - 1
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+    let mouseX = event.clientX - window.innerWidth / 2
+    let mouseY = event.clientY - window.innerHeight / 2
+    this.camera.position.x += (mouseX - this.camera.position.x) * 0.01
+    this.camera.position.y += (mouseY - this.camera.position.y) * 0.01
+    this.camera.lookAt(this.scene.position)
+  }
+
+  onDocumentMouseDown() {
+    event.preventDefault()
+    this.mouse.x = event.clientX / window.innerWidth * 2 - 1
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+
+    this.raycaster.setFromCamera(this.mouse, this.camera)
+
+    var intersects = this.raycaster.intersectObjects(
+      this.universeGroup.children
+    )
+    if (intersects.length > 0) {
+      // change this to single planets view
+      window.open('http://google.com')
+    }
+  }
+
+  // === resizes scene if browser window size changes ===
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+
+  createUniverse() {
+    const universeGroup = new THREE.Object3D()
 
     // === !!! IMPORTANT !!! ===
     // === add everything to the scene ===
-    scene.add(
+    universeGroup.add(
       starBackground,
       earth,
       proxima,
@@ -76,13 +141,9 @@ export default class Space extends React.Component {
       yzCetiD,
       kapteynC
     )
-    renderer.setClearColor('#000000')
-    renderer.setSize(width, height)
 
-    // === binds threejs variables to react component ===
-    this.scene = scene
-    this.camera = camera
-    this.renderer = renderer
+    this.scene.add(universeGroup)
+    this.universeGroup = universeGroup
 
     // === !!! IMPORTANT !!! ===
     // === bind objects imported from /planets ===
@@ -96,19 +157,6 @@ export default class Space extends React.Component {
     this.yzCetiC = yzCetiC
     this.yzCetiD = yzCetiD
     this.kapteynC = kapteynC
-
-    // === appends scene to the DOM ===
-    this.mount.appendChild(this.renderer.domElement)
-
-    // === orbit controls allows user to navigate 3D space with mouse ===
-    const controls = new OrbitControls(camera, renderer.domElement)
-
-    this.start()
-  }
-
-  componentWillUnmount() {
-    this.stop()
-    this.mount.removeChild(this.renderer.domElement)
   }
 
   start() {
@@ -146,6 +194,32 @@ export default class Space extends React.Component {
   }
 
   renderScene() {
+    // === ray caster !!! ===
+    // update the picking ray with the camera and mouse position
+    this.raycaster.setFromCamera(this.mouse, this.camera)
+
+    // calculate objects intersecting the picking ray
+    let intersects = this.raycaster.intersectObjects(
+      this.universeGroup.children
+    )
+
+    if (intersects.length > 0) {
+      if (this.INTERSECTED != intersects[0].object) {
+        if (this.INTERSECTED)
+          this.INTERSECTED.material.emissive.setHex(this.INTERSECTED.currentHex)
+        this.INTERSECTED = intersects[0].object
+        this.INTERSECTED.currentHex = this.INTERSECTED.material.emissive.getHex()
+        this.INTERSECTED.material.emissive.setHex(
+          Math.random() * 0xff00000 - 0xff00000
+        )
+      }
+    } else {
+      if (this.INTERSECTED)
+        this.INTERSECTED.material.emissive.setHex(this.INTERSECTED.currentHex)
+      this.INTERSECTED = null
+    }
+
+    // render scene
     this.renderer.render(this.scene, this.camera)
   }
 
