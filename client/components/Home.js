886b16452f14397352876b4b22108152a0691304
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-29 22:55:48 -0500

    home styling

--- client/components/Home.js
+++ client/components/Home.js
@@ -5,9 +5,21 @@ const Home = () => {
   return (
     <div id="homeContainer">
       <h1>BEAM UP</h1>
-      <p>an ethereal 3D journey through space and time</p>
-      <p>travel through the cosmos and discover exoplanets</p>
-      <p>use your mouse to orbit around our dreamy universe</p>
+      <div id="homeText">
+        <p>an ethereal 3D journey through space and time</p>
+        <p>
+          travel through the cosmos and discover exoplanets for human habitation
+        </p>
+        <p>use your mouse to orbit around our dreamy universe</p>
+        <br />
+        <p>maybe write some more nice words here my lovely teammates</p>
+        <p>
+          u r all smart, strong, talented, n beautiful. #beamteam4lyfe. jus
+          tryna fill in some space cos i don't feel like writing a real intro /
+          description
+        </p>
+        <p>hey remember when our app was about collecting garbage</p>
+      </div>
       <Link to="/space">
         <button type="button">START EXPLORING</button>
       </Link>
