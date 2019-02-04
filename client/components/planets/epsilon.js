4ac9ea6f4ddea4d842fd5ee0a30dc75a0345826e
MCClarke <mcarlotclarke@gmail.com>
2019-02-03 21:59:36 -0500

    added epsilon ring, needs correct texture

--- client/components/planets/epsilon.js
+++ client/components/planets/epsilon.js
@@ -1,11 +1,27 @@
 import * as THREE from '../../../three'
 
-const geometry = new THREE.SphereGeometry(11.2, 100, 90)
+const epsilonRadius = 11.2
+const geometry = new THREE.SphereGeometry(epsilonRadius, 100, 90)
 const planetTexture = new THREE.TextureLoader().load('/images/purple.png')
 planetTexture.wrapS = planetTexture.wrapT = THREE.MirroredRepeatWrapping
 planetTexture.repeat.set(2, 2)
 const material = new THREE.MeshBasicMaterial({map: planetTexture})
 const epsilon = new THREE.Mesh(geometry, material)
+
+const ringStart = epsilonRadius + 3.3
+const ringEnd = epsilonRadius + 10
+const ringTexture = new THREE.TextureLoader().load('/images/epsilonRing.jpg')
+const ringMaterial = new THREE.MeshBasicMaterial({
+  map: ringTexture,
+  side: THREE.DoubleSide
+})
+const ring = new THREE.Mesh(
+  new THREE.RingGeometry(ringStart, ringEnd, 30),
+  ringMaterial
+)
+epsilon.add(ring)
+ring.rotation.x = 90 * Math.PI / 180
+
 epsilon.name = 'Epsilon Eridani b'
 
 export default epsilon
