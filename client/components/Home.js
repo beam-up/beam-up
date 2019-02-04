87ba565f3de4f1e04df98377f480d56dcced6539
Ang Li <alizerosannihachi@gmail.com>
2019-02-04 16:17:05 -0500

    add moving particles at opening and ending screen

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {Animated} from 'react-animated-css'
+import Particles from 'react-particles-js'
+
+const particleOpt = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true
+      }
+    }
+  }
+}
 
 const Home = () => {
   return (
     <div className="homeContainer">
+      <div id="particles">
+        <Particles
+          width='100vw'
+          height='100vh'
+          params={particleOpt}
+        />
+      </div>
       <Animated animationIn="slideInUp" animationOut="fadeOut" isVisible={true}>
         <h1>BEAM UP</h1>
       </Animated>
