c0529fec3398aae88cb838daf0f95d0b0da3a93c
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-02-05 15:42:35 -0500

    adds earth text to earth component

---
+++ client/components/EarthText.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Animated} from 'react-animated-css'
+
+export const EarthText = () => {
+  return (
+    <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+      <div id="earthText">
+        <h2>what a lovely planet we have...</h2>
+        <p>
+          In your travels, you did not encounter another exoplanet that is
+          suitable for human life, but don't be discouraged. We have this
+          beautiful planet where the existence of life is serendipitous on a
+          cosmic scale.
+        </p>
+        <p>
+          Our world orbits the sun at just the right distance – the “Goldilocks
+          zone” – which is neither too hot nor too cold for liquid water, an
+          essential for Earth-like life, to be present.
+        </p>
+        <p>
+          The Sun, our star, is pretty friendly - it doesn’t have strong stellar
+          solar winds, like Proxima Centauri, or a flaring/inconsistent
+          brightness, like YZ Ceti.
+        </p>
+        <p>
+          Earth is not near a debris disk, like Tau Ceti - so we don’t
+          constantly risk collisions from asteroids and meteors.
+        </p>
+        <p>
+          You are invited to leave your mark on the universe by making a wish,
+          as a memento of your travels.
+        </p>
+        <h2>...and it's the only one we've got</h2>
+        <Link to="/wish">
+          <button id="earthButton" type="button">
+            LET'S MAKE A WISH
+          </button>
+        </Link>
+      </div>
+    </Animated>
+  )
+}
