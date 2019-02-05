4d8fd4b1acd5ac0c2aca853b22089a82a0c0e9dd
MCClarke <mcarlotclarke@gmail.com>
2019-02-05 18:44:36 -0500

    tau Ceti debris ring

--- client/components/planets/tauCetiE.js
+++ client/components/planets/tauCetiE.js
@@ -1,6 +1,7 @@
 import * as THREE from '../../../three'
 
-const geometry = new THREE.SphereGeometry(3.93, 100, 90)
+const tauCetiEMass = 3.93
+const geometry = new THREE.SphereGeometry(tauCetiEMass, 100, 90)
 const planetTexture = new THREE.TextureLoader().load('/images/purple.png')
 planetTexture.wrapS = planetTexture.wrapT = THREE.MirroredRepeatWrapping
 planetTexture.repeat.set(2, 2)
